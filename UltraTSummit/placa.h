3610d0263ae8f736b4742021318b2223339dfb23
TRZN11 <arth2002@yahoo.com.br>
2026-09-24 15:32:32 -0300

    Clean up placa.h by removing unused code
    Removed unnecessary preprocessor directives and pin definitions.

--- UltraTSummit/placa.h
+++ UltraTSummit/placa.h
@@ -32,32 +32,3 @@ void setupPortas() {
   pinMode(S6, INPUT);
   pinMode(LED_STRIP, OUTPUT);
 }
-=======
-#ifndef placa_H
-#define placa_H
-/* ! os sensores do meio na frente tão ligados com esses "pinos" (não usar) ! */
-#define S0 VP
-#define S1 VIN
-/* ! ^^^^ ! */
-
-#define S2 34  //frontal direita
-#define S3 35  //frontal esquerda
-#define S4 32  //lateral direita
-#define S5 33  //lateral esquerda
-#define S6 25  //sensor de linha esquerda
-#define S7 27
-#define S8 14  //sensor de linha direita
-
-#define IR_PIN 15
-#define LED_STRIP 2
-
-#define D26 26
-#define BTN2 12
-
-#define MA1 19
-#define MA2 18
-#define MB1 4
-#define MB2 23
-
-#endif
->>>>>>> 69ec7d623fe2b7c4684ae94136b5de74043b8162
